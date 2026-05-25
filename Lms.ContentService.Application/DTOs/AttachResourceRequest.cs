ca6a8da30f5d28d544c73e18c1eb560c1fb09cda
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-25 17:00:58 +0200

    Add AttachResourceRequest DTO with FileId and ResourceType
    Introduced AttachResourceRequest class in the DTOs namespace, including FileId (Guid) and ResourceType (string, default "Document") properties.

---
+++ Lms.ContentService.Application/DTOs/AttachResourceRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Lms.ContentService.Application.DTOs;
+
+public class AttachResourceRequest
+{
+    public Guid FileId { get; set; }
+    public string ResourceType { get; set; } = "Document";
+}
