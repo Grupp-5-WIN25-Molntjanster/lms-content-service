b344da4b1182bae8779245d9b5192d67af67b9fe
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-25 17:08:52 +0200

    Add IFileServiceClient interface and FileMetadataDto class
    Introduced the Lms.ContentService.Application.Interfaces namespace with the IFileServiceClient interface for file existence and metadata retrieval. Added FileMetadataDto to represent file metadata details.

---
+++ Lms.ContentService.Application/Interfaces/IFileServiceClient.cs
@@ -0,0 +1,15 @@
+﻿namespace Lms.ContentService.Application.Interfaces;
+
+public interface IFileServiceClient
+{
+    Task<bool> FileExistsAsync(Guid fileId);
+    Task<FileMetadataDto?> GetFileMetadataAsync(Guid fileId);
+}
+
+public class FileMetadataDto
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public long FileSize { get; set; }
+}
